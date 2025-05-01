fec11b65e2233e93f241a81f668876a62e5d95d7
TOSHIBA <TOSHIBA@TOSHIBA>
2025-05-02 01:34:17 +0400

    Add project files.

---
+++ Function/Calculate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    public class Calculate
+    {
+        public static double Calculator(int a, int b, string op = "+")
+        {
+            if (op == "+") return a + b;
+            if (op == "-") return a - b;
+            if (op == "*") return a * b;
+            if (op == "/")
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    return 0;
+                }
+                return (double)a / b;
+            }
+
+
+            return a + b;
+        }
+    }
+}
---
+++ Function/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Function
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Addition: {num1} + {num2} = {Calculate.Calculator(num1, num2)}");
+            Console.WriteLine($"Subtraction: {num1} - {num2} = {Calculate.Calculator(num1, num2, "-")}");
+            Console.WriteLine($"Multiplication: {num1} * {num2} = {Calculate.Calculator(num1, num2, "*")}");
+            Console.WriteLine($"Division: {num1} / {num2} = {Calculate.Calculator(num1, num2, "/")}");
+        }
+    }
+}
